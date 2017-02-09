d7b03e57b3b7b9094f99a2e49fe8a9090cf3a11f
Renato <rjsilva@outlook.com>
2017-02-09 15:53:06 -0200

    ajustes

--- src/components/content-button-cacamba.js
+++ src/components/content-button-cacamba.js
@@ -9,46 +9,52 @@ class ContentButtonCacamba extends Component {
     console.log('constructor-button')
     super()
     this.statusAtual = 0
-    this.idCacamba = 0
-    this.listaCacambas = []
+    this.state = {
+      listaCacambas: []
+    }
   }
 
   componentWillMount () {
     console.log('componentWillMount-button')
-    let component = this
-
-    //this.setState({ listaCacambas: require('json!../cacambas.json') })
-    // Performing a GET request
-    // axios.get('./src/cacambas.json')
-    //       .then(function (response) {
-    //         component.setState({ listaCacambas: response.data })
-    //         console.log(response.data)
-    //         console.log(response.status) // ex.: 200
-    //       })
+    // this.setState({ listaCacambas: require('json!../cacambas.json') })
   }
 
   componentDidMount () {
-    this.statusAtual = setInterval(() => {
-      this.setState = ({ statusAtual: this.updateStatusAtual(this.idCacamba) })
-    }, 5000)
-    console.log('componentDidMount-button')
-  }
 
-  getStatusCacamba (status) {
-    switch (status) {
-      case 1:
-        return 'btn btn-info'
-      case 0:
-        return 'btn btn-success'
-      case 2:
-        return 'btn btn-warning'
-      default:
-        return ''
-    }
+    let selfContext = this
+    // Performing a GET request
+    axios.get('./src/cacambas.json')
+           .then(response => {
+             //const listaCacambas = response.data.map(obj => obj.data)
+             this.setState({ listaCacambas : response.data})
+             // console.log(response.data)
+             // console.log(response.status) // ex.: 200
+           })
+
+    this.statusAtual = setInterval(() => {
+      this.state.listaCacambas.map((cacamba, index) => (
+      this.statusAtual = this.updateStatusAtual(cacamba.idCacamba)
+      ))
+    }, 5000)
+
+    console.log('componentDidMount-button')
   }
 
   updateStatusAtual (idCacamba) {
     return this.getStatusCacamba((Math.floor(Math.random() * (2 - 0 + 1) + 0)))
+  }
+
+  getStatusCacamba (status) {
+      switch (status) {
+        case 1:
+          return 'btn btn-info'
+        case 0:
+          return 'btn btn-success'
+        case 2:
+          return 'btn btn-warning'
+        default:
+          return ''
+      }
   }
 
   getDadosCacamba (idCacamba) {
@@ -57,18 +63,17 @@ class ContentButtonCacamba extends Component {
 
   getCacambas () {
     // const listCacambas = require('json!../cacambas.json')
-    this.listaCacambas = require('json!../cacambas.json')
+    // this.listaCacambas = require('json!../cacambas.json')
     // for (var i = 1; i <= 50; i++) {
     //   listCacambas.push({ idCacamba: i, statusCacamba: (Math.floor(Math.random() * (2 - 0 + 1) + 0)) })
     // }
-
-    return this.listaCacambas.map((cacamba, index) => (
-      <ButtonCacamba
-        key={index}
-        text={cacamba.idCacamba}
-        handleClick={(e) => this.getDadosCacamba(cacamba.idCacamba)}
-        statusCacamba={this.getStatusCacamba(cacamba.statusCacamba)} />
-    ))
+     return this.state.listaCacambas.map((cacamba, index) => (
+       <ButtonCacamba
+         key={index}
+         text={cacamba.idCacamba}
+         handleClick={(e) => this.getDadosCacamba(cacamba.idCacamba)}
+         statusCacamba={this.getStatusCacamba(cacamba.statusCacamba)} />
+      ))
   }
 
   render () {
